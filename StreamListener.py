3a6b270121a7202911f862fc951af9797546ee50
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-04-14 22:01:10 +1000

    finish data collection

--- StreamListener.py
+++ StreamListener.py
@@ -1,49 +1,66 @@
 import tweepy
+import mysql.connector
 from mysql.connector import Error
 from dateutil import parser
 import os
+import json
+
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
+password = os.environ['PASSWORD']
 
 # Tweepy class to access Twitter API
 class Streamlistener(tweepy.StreamListener):
 	
-
 	def on_connect(self):
 		print("You are connected to the Twitter API")
 
-
 	def on_error(self):
 		if status_code != 200:
-			print("error found")
-			# returning false disconnects the stream
 			return False
 
-	"""
-	This method reads in tweet data as Json
-	and extracts the data we want.
-	"""
 	def on_data(self,data):
-		
+		"""Read in tweet data as json and do extraction
+		"""
 		try:
 			raw_data = json.loads(data)
 
 			if 'text' in raw_data:
-				 
+				# Get all data
 				username = raw_data['user']['screen_name']
 				created_at = parser.parse(raw_data['created_at'])
 				tweet = raw_data['text']
 				retweet_count = raw_data['retweet_count']
-
-				if raw_data['place'] is not None:
-					place = raw_data['place']['country']
-					print(place)
-				else:
-					place = None
-				
-
+				place = raw_data['place']['country'] if raw_data['place'] is not None else None
 				location = raw_data['user']['location']
-
 				#insert data just collected into MySQL database
-				connect(username, created_at, tweet, retweet_count, place, location)
+				self.connect(username, created_at, tweet, retweet_count, place, location)
 				print("Tweet colleted at: {} ".format(str(created_at)))
 		except Error as e:
-			print(e)
+			'Error collecting tweet data: {}'.format(e)
+
+	def connect(self, username, created_at, tweet, retweet_count, place , location):
+		"""Connect to MySQL database and insert twitter data
+		"""
+		cursor, con = None, None
+		try:
+			con = mysql.connector.connect(host='localhost',
+																		database='twitterdb', 
+																		user='root', 
+																		password=password, 
+																		charset='utf8',
+																		auth_plugin='mysql_native_password')
+			if con.is_connected():
+				cursor = con.cursor()
+				query = "INSERT INTO Golf (username, created_at, tweet, retweet_count,place, location) VALUES (%s, %s, %s, %s, %s, %s)"
+				cursor.execute(query, (username, created_at, tweet, retweet_count, place, location))
+				con.commit()
+				print('Data upload soccess.')
+		except Error as e:
+			print('Error uploading data: {}'.format(e))
+		if cursor:
+			cursor.close()
+		if con:
+			con.close()
+
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import mysql.connector
-from mysql.connector import Error
 import tweepy
-import json
 from dateutil import parser
 import time
 import os
-import subprocess
 from StreamListener import Streamlistener
 
 from dotenv import load_dotenv
@@ -15,42 +11,9 @@ consumer_key = os.environ['CONSUMER_KEY']
 consumer_secret = os.environ['CONSUMER_SECRET']
 access_token = os.environ['ACCESS_TOKEN']
 access_token_secret = os.environ['ACCESS_TOKEN_SECRET']
-password = os.environ['PASSWORD']
-
-
-def connect(username, created_at, tweet, retweet_count, place , location):
-	"""
-	connect to MySQL database and insert twitter data
-	"""
-	try:
-		con = mysql.connector.connect(host = 'localhost',
-		database='twitterdb', user='root', password = password, charset = 'utf8')
-		
-
-		if con.is_connected():
-			"""
-			Insert twitter data
-			"""
-			cursor = con.cursor()
-			# twitter, golf
-			query = "INSERT INTO Golf (username, created_at, tweet, retweet_count,place, location) VALUES (%s, %s, %s, %s, %s, %s)"
-			cursor.execute(query, (username, created_at, tweet, retweet_count, place, location))
-			con.commit()
-			
-			
-	except Error as e:
-		print(e)
-
-	cursor.close()
-	con.close()
-
-	return
-
 
 
 if __name__== '__main__':
-	print(consumer_key, consumer_secret)
-
 	# # #Allow user input
 	# track = []
 	# while True:
@@ -72,10 +35,11 @@ if __name__== '__main__':
 	api =tweepy.API(auth, wait_on_rate_limit=True)
 
 	# create instance of Streamlistener
-	listener = Streamlistener(api = api)
+	listener = Streamlistener(api=api)
 	stream = tweepy.Stream(auth, listener = listener)
 
 	track = ['golf', 'masters', 'reed', 'mcilroy', 'woods']
-	#track = ['nba', 'cavs', 'celtics', 'basketball']
 	# choose what we want to filter by
-	stream.filter(track = track, languages = ['en'])
+	stream.filter(track=track, languages=['en'])
+
+
